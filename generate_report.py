87185d4b5e67f7a49e268a5aac55d33b2e6884c3
BEBOLDER\Juan.valencia <juan.valencia@bebolder.co>
2025-03-14 10:50:46 -0500

    Add

--- generate_report.py
+++ generate_report.py
@@ -6,31 +6,24 @@ import csv
 import json
 from flask import jsonify, request, send_from_directory
 
-# Función para generar el reporte
 def generar_reporte(proyecto):
-    # Datos de autenticación y configuración
     email = "pablo.munoz@bebolder.co"
     api_token = "ATATT3xFfGF0UrsshZ9JZEMG-0eZQbBJ_GgT5-mSghYU8ustURw07LprkdngoC1iO8Y198B4b8nIePIekKQNhL4uQAQsVeIXhpWRY3GjtN_O-j9zOS_ZixxwZPdzcqdVsw_SKfox8okJwcj_57XIu3ZM0C7iwDFD3E-vnkLo6TQpfL-i_3mV6jM=07B92C26"
     jira_domain = "https://bebolder.atlassian.net"
 
-    # Autenticación base64
     auth_string = f"{email}:{api_token}"
     encoded_auth = base64.b64encode(auth_string.encode()).decode()
 
-    # Parámetros de la consulta
     start_at = 0
     max_results = 100
     total = 0
 
-    # Definir el JQL query
     jql_query = f"project={proyecto}"
     
-    # Eliminar el archivo CSV existente si existe
     archivo_csv = f"data/issues_{proyecto}.csv"
     if os.path.exists(archivo_csv):
-        os.remove(archivo_csv)  # Eliminar el archivo CSV
+        os.remove(archivo_csv)
 
-    # Función para obtener los issues
     def obtener_issues_jira():
         nonlocal start_at, total
         while start_at < total or total == 0:
@@ -41,23 +34,19 @@ def generar_reporte(proyecto):
             }
             
             try:
-                # Se añade el parámetro timeout=180 para esperar hasta 3 minutos
-                response = requests.get(url, headers=headers, timeout=300)  # Timeout de 3 minutos
-                response.raise_for_status()  # Si la respuesta tiene un error, levantará una excepción
+                response = requests.get(url, headers=headers, timeout=130)
+                response.raise_for_status()
                 json_response = response.json()
                 total = json_response['total']
                 
-                # # Guardar los datos JSON en un archivo
-                # with open(f"data/issues_{proyecto}.json", "a") as json_file:
-                #     json.dump(json_response, json_file, indent=4)  # Escribe la respuesta JSON de manera legible
+                with open(f"data/issues_{proyecto}.json", "a") as json_file:
+                    json.dump(json_response, json_file, indent=4)
                 
-                # Escribir los datos en el archivo CSV
                 with open(f"data/issues_{proyecto}.csv", mode='a', newline='', encoding='utf-8') as file:
                     writer = csv.writer(file)
                     
-                    # Escribir solo los encabezados una vez
                     if start_at == 0:
-                        writer.writerow(["Proyecto", "Clave/ID", "Tipo issue", "Prioridad", "Informador", "Creada", "Titulo", "Estado", "Responsable", "Actualizada", "Sprint", "Definición de hecho", "Puntos de historia", "Puntos estimados", "Puntos ejecutados", "Sumatoria tiempo empleado", "Seguimiento de tiempo HH/MM", "Codigo externo"])
+                        writer.writerow(["Proyecto", "Clave/ID", "Tipo issue", "Prioridad", "Informador", "Creada", "Titulo", "Estado", "Responsable", "Actualizada", "Cantidad de sprint", "Sprint", "Definición de hecho", "Puntos de historia", "Puntos estimados", "Puntos ejecutados", "Sumatoria tiempo empleado", "Seguimiento de tiempo HH/MM", "Codigo externo"])
 
                     for issue in json_response['issues']:
                         project = issue['fields']['project']['name']
@@ -82,11 +71,21 @@ def generar_reporte(proyecto):
                         update = issue['fields'].get('update', None) 
                         if not update: 
                             update = "Sin actualizar"
-                        sprint = issue['fields'].get('customfield_10020', None) 
-                        if not sprint: 
-                            sprint = "N/A"
-                        definitionOfFact = issue['fields'].get('customfield_10048', None) 
-                        if not definitionOfFact: 
+                        amountSprint = issue['fields'].get('customfield_10020', None) 
+                        if amountSprint is None or len(amountSprint) == 0:
+                            amountSprint = "Sin sprint"
+                        else:
+                            amountSprint = f"{len(amountSprint)} Sprints"
+                        sprint = issue['fields'].get('customfield_10020', None)
+                        if sprint is None or len(sprint) == 0:
+                            sprint = "Sin sprint"
+                        else:
+                            sprint_names = [item['name'] for item in sprint if 'name' in item]
+                            sprint = ", ".join(sprint_names)
+                        definitionOfFact = issue['fields'].get('customfield_10048', None)
+                        if isinstance(definitionOfFact, dict) and 'value' in definitionOfFact:
+                            definitionOfFact = definitionOfFact['value']
+                        else:
                             definitionOfFact = "N/A"
                         storyPoint = issue['fields'].get('customfield_10033', None) 
                         if not storyPoint: 
@@ -111,9 +110,8 @@ def generar_reporte(proyecto):
                         if not externalCode: 
                             externalCode = "N/A"
 
-                        # Añadimos la fila con los datos
-                        writer.writerow([project, key, issuetype, priority, reporter, created, summary, status, assignee_name, update, sprint, definitionOfFact, storyPoint, storyPointEstimated, storyPointExecuted, aggregatetimespent, seguimientotiempo, externalCode])
-                
+                        writer.writerow([project, key, issuetype, priority, reporter, created, summary, status, assignee_name, update, amountSprint, sprint, definitionOfFact, storyPoint, storyPointEstimated, storyPointExecuted, aggregatetimespent, seguimientotiempo, externalCode])
+
                 start_at += max_results
 
             except requests.exceptions.RequestException as e:
@@ -121,35 +119,26 @@ def generar_reporte(proyecto):
 
         print(f"Datos extraídos exitosamente para el proyecto {proyecto}")
 
-    # Medir el tiempo de ejecución
     start_time = time.time()
 
-    # Llamar a la función para obtener los issues
     obtener_issues_jira()
 
-    # Calcular el tiempo que tardó el proceso
     end_time = time.time()
     execution_time = end_time - start_time
 
-    # Devolver la URL pública del archivo y el tiempo de ejecución
     return {"archivo": f"https://testjira.onrender.com/data/issues_{proyecto}.csv", "tiempo": execution_time}
 
-# Ruta para manejar la generación del reporte y servir el archivo
 def generar_reporte_route(app):
     @app.route('/generar_reporte', methods=['POST'])
     def reporte_route():
-        data = request.get_json()  # Obtener los datos enviados por el formulario
+        data = request.get_json()  
         proyecto = data['proyecto']
 
-        # Llamamos a la función generar_reporte de generate_report.py
         resultado = generar_reporte(proyecto)
 
-        # Devolver la URL pública del archivo y el tiempo de ejecución
         return jsonify(resultado)
 
-    # Ruta para servir los archivos CSV desde la carpeta 'data'
     @app.route('/data/<path:filename>')
     def serve_file(filename):
-        # Sirve el archivo CSV desde la carpeta 'data' sin forzar la descarga
         return send_from_directory(os.path.join(app.root_path, 'data'), filename, as_attachment=False)
 
